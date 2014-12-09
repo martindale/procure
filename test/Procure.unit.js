c9341bcdb701a9ea90ec55578f8bfeefd676a363
Eric Martindale <eric@ericmartindale.com>
2014-12-08 20:57:06 -0500

    Build first draft of procure.

---
+++ test/Procure.unit.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+var procure = require('../');
+
+describe('Procure', function() {
+  
+  it('should procure local files', function( done ) {
+    procure('./test/fixtures/test.json', function( err , content ) {
+      assert.ok( content.length );
+      done();
+    });
+  });
+  
+  it('should procure remote files', function( done ) {
+    procure('http://www.google.com/', function( err , content ) {
+      assert.ok( content.length );
+      done();
+    });
+  });
+  
+  it('should not procure non-existent local files', function( done ) {
+    procure('./test/fixtures/nope.json', function( err , content ) {
+      assert.ok( err && !content );
+      done();
+    });
+  });
+  
+  it('should not procure non-existent remote files', function( done ) {
+    procure('http://www.google.com/this-is-a-test-request', function( err , content ) {
+      assert.ok( err && !content );
+      done();
+    });
+  });
+
+});
